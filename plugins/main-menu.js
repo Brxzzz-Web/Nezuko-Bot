bc1ceb0be7cf4ae0280243ff03216b9d3ce029bc
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-09-12 21:48:10 -0500

    main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -24,41 +24,37 @@ Crea un *Sub-Bot* con tu número utilizando *#qr* o *#code*
 
 ❍ Comandos para ver estado e información de la Bot.
 ᰔᩚ *#help • #menu*
-> ✦ Ver la lista de comandos de la Bot.
+> ✦ Ver la lista de comandos del Bot.
 ᰔᩚ *#uptime • #runtime*
-> ✦ Ver tiempo activo o en linea de la Bot.
-ᰔᩚ *#sc • #script*
-> ✦ Link del repositorio oficial de la Bot
-ᰔᩚ *#staff • #colaboradores*
-> ✦ Ver la lista de desarrolladores de la Bot.
+> ✦ Ver tiempo activo o en linea del Bot.
 ᰔᩚ *#serbot • #serbot code*
 > ✦ Crea una sesión de Sub-Bot.
 ᰔᩚ *#bots • #sockets*
 > ✦ Ver la lista de Sub-Bots activos.
 ᰔᩚ *#status • #estado*
-> ✦ Ver el estado actual de la Bot.
+> ✦ Ver el estado actual del Bot.
 ᰔᩚ *#links • #grupos*
-> ✦ Ver los enlaces oficiales de la Bot.
+> ✦ Ver los enlaces oficiales del Bot.
 ᰔᩚ *#infobot • #infobot*
-> ✦ Ver la información completa de la Bot.
+> ✦ Ver la información completa del Bot.
 ᰔᩚ *#sug • #newcommand*
 > ✦ Sugiere un nuevo comando.
 ᰔᩚ *#p • #ping*
 > ✦ Ver la velocidad de respuesta del Bot.
 ᰔᩚ *#reporte • #reportar*
-> ✦ Reporta alguna falla o problema de la Bot.
+> ✦ Reporta alguna falla o problema del Bot.
 ᰔᩚ *#sistema • #system*
 > ✦ Ver estado del sistema de alojamiento.
 ᰔᩚ *#speed • #speedtest*
-> ✦ Ver las estadísticas de velocidad de la Bot.
+> ✦ Ver las estadísticas de velocidad del Bot.
 ᰔᩚ *#views • #usuarios*
 > ✦ Ver la cantidad de usuarios registrados en el sistema.
 ᰔᩚ *#funciones • #totalfunciones*
-> ✦ Ver todas las funciones de la Bot.
+> ✦ Ver todas las funciones del Bot.
 ᰔᩚ *#ds • #fixmsgespera*
 > ✦ Eliminar archivos de sesión innecesarios.
 ᰔᩚ *#editautoresponder*
-> ✦ Configurar un Prompt personalizado de la Bot.
+> ✦ Configurar un Prompt personalizado del Bot.
 
 • :･ﾟ⊹˚• \`『 Buscadores 』\` •˚⊹:･ﾟ•
 
