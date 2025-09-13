0f180c6efd897bc42c3c75b0b4fabfe82b0e9f28
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-09-12 21:30:00 -0500

    main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -37,7 +37,7 @@ global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
 
 global.canalIdM = ["120363401983007420@newsletter", "120363401983007420@newsletter"]
-global.canalNombreM = ["ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥙ⍴ძᥲ𝗍ᥱs ❀", "ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥲ᥎іs᥆s ❀"]
+global.canalNombreM = ["⏤͟͟͞͞ᰔᩚ 𝙽𝚎𝚣𝚞𝚔𝚘 𝙱𝚘𝚝-𝚄𝚙𝚍𝚊𝚝𝚎𝚜 ⏤͟͟͞͞ᰔᩚ", "⏤͟͟͞͞ᰔᩚ 𝙽𝚎𝚣𝚞𝚔𝚘 𝙱𝚘𝚝-𝚄𝚙𝚍𝚊𝚝𝚎𝚜 ⏤͟͟͞͞ᰔᩚ"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
