e24b0aab898ec318aeed37d2ad333348aa78e6d4
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-09-28 14:53:39 -0500

    settings.js

--- settings.js
+++ settings.js
@@ -56,6 +56,7 @@ ch1: "120363401983007420@newsletter"
 
 global.APIs = {
 xyro: { url: "https://xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
 vreden: { url: "https://api.vreden.web.id", key: null },
 delirius: { url: "https://api.delirius.store", key: null },
 zenzxz: { url: "https://api.zenzxz.my.id", key: null },
