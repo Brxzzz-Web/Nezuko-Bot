da9bfa1f17846a75c546a102b269b110a8c4ae88
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-10-01 17:16:58 -0500

    group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -6,9 +6,9 @@ try {
 const groupInfo = await conn.groupMetadata(m.chat)
 const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
 const ownerBot = global.owner[0][0] + '@s.whatsapp.net'
-if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ No puedo eliminar el bot del grupo.`, m)
-if (user === ownerGroup) return conn.reply(m.chat, `ꕥ No puedo eliminar al propietario del grupo.`, m)
-if (user === ownerBot) return conn.reply(m.chat, `ꕥ No puedo eliminar al propietario del bot.`, m)
+if (user === conn.user.jid) return conn.reply(m.chat, `❀ No puedo eliminar el bot del grupo.`, m)
+if (user === ownerGroup) return conn.reply(m.chat, `❀ No puedo eliminar al propietario del grupo.`, m)
+if (user === ownerBot) return conn.reply(m.chat, `❀ No puedo eliminar al propietario del bot.`, m)
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
 } catch (e) {
 conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
