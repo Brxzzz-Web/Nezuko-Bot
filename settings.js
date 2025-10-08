46f50e7977735854a837e6d9a857b5be4d4e47de
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-10-08 12:30:29 -0500

    settings.js

--- settings.js
+++ settings.js
@@ -49,7 +49,7 @@ global.channel = "https://whatsapp.com/channel/0029Vb5l5w1CHDyjovjN8s2V"
 global.github = "https://github.com/"
 global.gmail = "jxxlznexe@gmail.com"
 global.ch = {
-ch1: "120363401404146384@newsletter"
+ch1: "120363401983007420@newsletter"
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
